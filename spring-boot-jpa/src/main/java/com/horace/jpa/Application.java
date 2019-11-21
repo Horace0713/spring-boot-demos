a833df96f0e21e7ccd8d6cbd48a149a07c2d6250
Horace0713 <dreamygh@163.com>
2019-11-21 22:26:48 +0800

    init

---
+++ spring-boot-jpa/src/main/java/com/horace/jpa/Application.java
@@ -0,0 +1,17 @@
+package com.horace.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: Horace
+ * @desc:
+ * @project: spring-boot-demos
+ * @create: 2019-11-21 22:09
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ spring-boot-jpa/src/main/java/com/horace/jpa/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.horace.jpa.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: Horace
+ * @desc:
+ * @project: spring-boot-demos
+ * @create: 2019-11-21 22:16
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello world";
+    }
+}
