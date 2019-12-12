ebe23857202eed5ba984517f243886effae638f7
Horace0713 <dreamygh@163.com>
2019-12-12 21:40:59 +0800

    add test

---
+++ spring-boot-config/src/main/java/com/horace/config/PersonValue.java
@@ -0,0 +1,71 @@
+package com.horace.config;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author: Horace
+ * @desc:
+ * @project: spring-boot-demos
+ * @create: 2019-12-11 21:35
+ */
+//@ConfigurationProperties(prefix = "person")
+@Component
+@Slf4j
+@Data
+public class PersonValue {
+
+    /**
+     * <bean class="Person">
+     *      <property name="lastName" value="
+     *      1.字面量/
+     *      2.${key}从环境变量、配置文件中获取值/
+     *      3.#{SpEL}"></property>  spring EL表达式
+     * <bean/>
+     */
+
+    @Value("${person.last-name}") // 从环境变量、配置文件中获取值
+//    @Value("${person.lastName}")  todo 不支持松散绑定，lastName既能取到lastName，又能取到last-name，叫做松散绑定，ConfigurationProperties能做到松散绑定
+    private String lastName;
+
+    @Value("#{10*2}")
+    private Integer age; //   spring EL表达式
+
+    @Value("true") // 字面量
+    private Boolean boss;
+
+    private Date birth;
+
+    private String email;
+
+    private String shuangyinhao;
+
+    private String danyinhao;
+
+    private String bigStr;
+
+    private Map<String, Object> maps;
+
+    private Map<String, Object> maps2;
+
+    private List<Object> lists;
+
+    private Dog dog;
+
+    private List<Dog> dogList;
+
+    private Map<String,List<Dog>> dogMap;
+
+    @PostConstruct
+    public void init() {
+        log.info("{} {}", this.getClass().getSimpleName(), this);
+    }
+}
---
+++ spring-boot-config/src/test/java/com/horace/config/ConfigTest.java
@@ -0,0 +1,45 @@
+package com.horace.config;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+/**
+ * @author: Horace
+ * @desc:
+ * @project: spring-boot-demos
+ * @create: 2019-12-12 20:54
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class ConfigTest {
+
+    @Autowired
+    private PersonValue personValue;
+    @Autowired
+    private PersonConfig config;
+
+    @Test
+    public void personValue(){
+        Assert.assertNotNull(personValue.getLastName());
+        Assert.assertNotNull(personValue.getAge());
+        Assert.assertEquals(personValue.getBoss(),true);
+        assertThat(personValue.getAge()).isEqualTo(20);
+    }
+
+    @Test
+    public void personConfig(){
+        assertThat(config.getAge()).isEqualTo(18);
+        assertThat(config.getLists().size()).isEqualTo(2);
+        assertThat(config.getShuangyinhao()).contains("\n");
+        assertThat(config.getDanyinhao()).doesNotContain("\n");
+        assertThat(config.getDanyinhao()).contains("\\n");
+    }
+}
