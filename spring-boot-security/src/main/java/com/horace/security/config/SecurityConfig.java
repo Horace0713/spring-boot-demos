4f8268c53b48f6d650fcd1fa37295395ec27a604
Horace0713 <dreamygh@163.com>
2019-12-15 19:19:46 +0800

    security 配置用户名和密码

--- spring-boot-security/src/main/java/com/horace/security/config/SecurityConfig.java
+++ spring-boot-security/src/main/java/com/horace/security/config/SecurityConfig.java
@@ -3,59 +3,29 @@ package com.horace.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  * @author: Horace
- * @desc: securiy 配置
+ * @desc:
  * @project: spring-boot-demos
- * @create: 2019-11-24 21:44
+ * @create: 2019-12-15 18:47
  */
 @Configuration
-public class SecurityConfig {
-//    @Bean
-//    public WebSecurityConfigurerAdapter webSecurityConfigurerAdapter() {
-//        return new WebSecurityConfigurerAdapter() {
-//            @Override
-//            protected void configure(HttpSecurity http) throws Exception {
-//                http.authorizeRequests()
-//                        .anyRequest().permitAll();//所有请求全部放行
-//            }
-//        };
-//    }
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
-    public WebSecurityConfigurerAdapter webSecurityConfigurerAdapter() {
-        return new WebSecurityConfigurerAdapter() {
-            @Override
-            protected void configure(HttpSecurity http) throws Exception {
-                http.authorizeRequests()
-                        .antMatchers("/horace/*").permitAll() // 访问'/horace/*'一律放行
-//                        .antMatchers("/hello/*").hasRole("admin")
-                        .anyRequest().authenticated(); //其他的都要登录
-
-            }
-
-//            @Override
-//            protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//                        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-//
-//                auth
-//                        .inMemoryAuthentication()
-//                        .passwordEncoder(new BCryptPasswordEncoder())
-//                        .withUser("horace")
-//                        .password("123")
-//                        .roles("admin");
-//            }
-        };
+    PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance(); //如果没有的话，会报java.lang.IllegalArgumentException: There is no PasswordEncoder mapped for the id "null"
     }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception { //这是基于内存的配置，则yml中配置的不生效
+        auth.inMemoryAuthentication()
+                .withUser("admin3").password("admin3").roles("ADMIN", "USER")
+                .and()
+                .withUser("user").password("123").roles("USER");
+    }
 }
