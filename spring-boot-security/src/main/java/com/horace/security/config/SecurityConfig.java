aac7986ca5bb9712a8e12211c589249a3ea2af4f
Horace0713 <dreamygh@163.com>
2019-12-15 22:57:45 +0800

    登录 登出

--- spring-boot-security/src/main/java/com/horace/security/config/SecurityConfig.java
+++ spring-boot-security/src/main/java/com/horace/security/config/SecurityConfig.java
@@ -6,8 +6,20 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @author: Horace
@@ -42,7 +54,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/user/**").access("hasAnyRole('ADMIN','USER')") //或
                 .antMatchers("/db/**").access("hasRole('ADMIN') and hasRole('DBA')") //和
                 .anyRequest().authenticated() //其他url都必须登录访问
-                .and().formLogin().loginProcessingUrl("/login").permitAll()
-                .and().csrf().disable(); //关闭csrf
+                .and().formLogin().loginProcessingUrl("/login")
+                .usernameParameter("name").passwordParameter("password")
+                .successHandler(new AuthenticationSuccessHandler() {
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+                        //登录成功返回给前端的json
+                    }
+                })
+                .failureHandler(new AuthenticationFailureHandler() {
+                    @Override
+                    public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+                        //登录失败返回给前端的json
+                    }
+                }).permitAll()
+                .and().logout().logoutUrl("/login").clearAuthentication(true).invalidateHttpSession(true).addLogoutHandler(new LogoutHandler() {
+            @Override
+            public void logout(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) {
+
+            }
+        }).logoutSuccessHandler(new LogoutSuccessHandler() {
+            @Override
+            public void onLogoutSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+                httpServletResponse.sendRedirect("/login_page");
+            }
+        });
     }
 }
