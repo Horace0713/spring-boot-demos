c154266b3dccb9c84292478e65a0677d6f278aa6
Horace0713 <dreamygh@163.com>
2020-01-05 21:22:31 +0800

    基本实现1个TxId查各服务的日志 及其总结

--- spring-boot-web/src/main/java/com/horace/web/service/BookServce.java
+++ spring-boot-web/src/main/java/com/horace/web/service/BookServce.java
@@ -25,6 +25,16 @@ public class BookServce {
     }
 
     public String jpa() {
+        /**
+         * 关于实现  用1个TxId 查一次请求 在不同服务上log 的总结
+         * 1. eureka ：
+         * 2. gateway ：转发A B的服务，负载均衡，并注册到eureka，引入sleuth服务
+         * 3. A 服务：注册到eureka，引入sleuth服务，spring.application.name , servlet.context-path都要配，A调B服务，
+         *      必须自定义Restemplate，并且必须加@LoadBalanced，否则不走door，各服务TxId不一样
+         * 4. B 服务：注册到eureka，引入sleuth服务，spring.application.name , servlet.context-path都要配
+         * 5. todo logback.xml 明确TxId在日志中的格式
+         * 6. todo accesslog 在tomcat中配，带上TxId
+         */
         String result = restTemplate.getForObject("http://SPRING-BOOT-JPA/jpa/rest/hello", String.class);
         return result;
     }
