8723fa5c2b9432cf60045ad61ff746e483239516
Horace0713 <dreamygh@163.com>
2020-01-05 19:50:24 +0800

    log 打印TxId，但不完善，需补充logback.xml

--- spring-boot-web/src/main/java/com/horace/web/web/HelloController.java
+++ spring-boot-web/src/main/java/com/horace/web/web/HelloController.java
@@ -1,5 +1,6 @@
 package com.horace.web.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @create: 2019-11-21 22:16
  */
 @RestController
+@Slf4j
 public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
+        log.info("hello");
         return "hello world";
     }
 }
