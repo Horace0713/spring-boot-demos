05b557b6aa4a71c43bcc9c76ff2d1c2640999aa7
Horace0713 <dreamygh@163.com>
2020-01-05 22:12:19 +0800

    1个TxId也能查到异步线程池的log

--- spring-boot-retry/src/main/java/com/horace/retry/RetryApplication.java
+++ spring-boot-retry/src/main/java/com/horace/retry/RetryApplication.java
@@ -14,8 +14,8 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 @EnableAsync
 @EnableRetry
-public class Application {
+public class RetryApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(RetryApplication.class, args);
     }
 }
